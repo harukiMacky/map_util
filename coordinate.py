b7c823baf668b3ddf430a5fdabd17f0ceada7638
Haruki Makisaka <haruki.0214.rmb@gmail.com>
2024-03-13 21:40:29 +0900

    Attribute名変更

--- coordinate.py
+++ coordinate.py
@@ -6,18 +6,18 @@ class LongitudeLatitudePoint:
 
     Attributes
     ----------
-    x : float | int
-        経度。東経を正の数とする。
-    y : float | int
+    latitude : float | int
         緯度。北緯を正の数とする。
+    longitude : float | int
+        経度。東経を正の数とする。
     """
 
-    def __init__(self, x: float | PointDMS, y: float | PointDMS) -> None:
+    def __init__(self, latitude: float | PointDMS, longitude: float | PointDMS) -> None:
         """コンストラクタ
         DEGもしくはDNS形式の緯度・経度を設定する。
         """
-        if type(x) == type(y):
-            self.x = x
-            self.y = y
+        if type(latitude) == type(longitude):
+            self.latitude = latitude
+            self.longitude = longitude
         else:
             raise TypeError("座標はDEGもしくはDNS表記で統一してください。")

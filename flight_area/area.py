5498874fd85779253148307a5f74887f344fb596
Haruki Makisaka <haruki.0214.rmb@gmail.com>
2024-03-14 23:43:39 +0900

    Polygonの始点と終点を一致させる実装を追加

--- flight_area/area.py
+++ flight_area/area.py
@@ -15,7 +15,9 @@ class Area:
         最高高度
     """
 
-    def __init__(self, name: str, point_list: list, maximum_altitude: int) -> None:
+    def __init__(
+        self, name: str, point_list: list[list], maximum_altitude: int
+    ) -> None:
         self.name = name
         self.point_list = point_list
         self.maximum_altitude = maximum_altitude
@@ -24,6 +26,10 @@ class Area:
         """
         Geojsonクラス作成
         """
+        # 始点と終点を一致させる
+        if self.point_list[0] != self.point_list[-1]:
+            self.point_list.append(self.point_list[0])
+
         area_info = self.name + " : " + self.maximum_altitude + " ft"
 
         return Polygon(coordinates=[self.point_list], properties={"name": area_info})

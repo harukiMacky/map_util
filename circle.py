f7cd62db8ae230f1cc8a4d727c20e97632089f78
Haruki Makisaka <haruki.0214.rmb@gmail.com>
2024-03-14 22:43:26 +0900

    バグ修正、コメント記入
    Polygonデータの生成部分を修正

--- circle.py
+++ circle.py
@@ -4,21 +4,40 @@ import json
 
 
 class CircleZone:
-
     def __init__(self, radius: float, center: Point) -> None:
+        """円を定義する
+        Parameters
+        -----------
+        radius : float
+            半径
+        center : Point
+            中心の座標（緯度経度）
+        """
         self.radius = radius
         self.center = center
 
     def get_polygon(self, precision: float = 0.01) -> Polygon:
+        """円形のPolygonデータ生成
+        円周上の点を計算し、Polygonデータを作成する。
+
+        Parameter
+        ----------
+        precision : float
+            点の間隔
+
+        Returns
+        ---------
+        Geojson形式のPolygonデータ
+        """
         theta = 0
         point_list = []
         while theta < 2 * numpy.pi:
             x = self.center["coordinates"][0] + self.radius * numpy.cos(theta)
             y = self.center["coordinates"][1] + self.radius * numpy.cos(theta)
             point_list.append([x, y])
-            theta += precision  # これくらいで十分？
+            theta += precision
 
-        return Polygon(coordinates=point_list)
+        return Polygon(coordinates=[point_list])
 
 
 circle = CircleZone(5, Point([0, 0]))

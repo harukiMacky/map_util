db1c4cdc18622caa715cf7cd1e26291323850fe2
Haruki Makisaka <haruki.0214.rmb@gmail.com>
2024-03-20 14:05:05 +0900

    コメント追加

--- flight_area/area.py
+++ flight_area/area.py
@@ -1,26 +1,39 @@
 from geojson import Polygon
+from geopy import Point
 
 
 class Area:
-    """1フライトエリア
+    """
     フライトエリア内の一部を表すクラス。
 
     Attributes
     -----------
     name : str
         エリア名（エリアAなど）
-    point_list : list
+    point_list : list[geopy.Point]
         エリア周囲のポイントのリスト
     maximum_altitude : int
         最高高度
     """
 
     def __init__(
-        self, name: str, point_list: list[list], maximum_altitude: int
+        self, name: str, point_list: list[Point], maximum_altitude: int
     ) -> None:
         self.name = name
-        self.point_list = point_list
+        self.point_list = self._convert_point_for_geojson(point_list)
         self.maximum_altitude = maximum_altitude
+
+    def _convert_point_for_geojson(self, point_list) -> list:
+        """
+        ポイントをgeojsonように変換する。
+        """
+        converted_point_list = []
+        for p in point_list:
+            point_str = p.decimal_format()
+            point_str = point_str.replace(" ", "").split(",")
+            converted_point_list.append([float(point_str[1]), float(point_str[0])])
+
+        return converted_point_list
 
     def make_area(self) -> Polygon:
         """
